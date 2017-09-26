36ea9bebaaa68068bf12417eb616213b98bfdef2
njupt <18362971301@163.com>
2017-09-26 16:59:32 +0800

    second commit

--- src/com/NiukeWang/program/Test.java
+++ src/com/NiukeWang/program/Test.java
@@ -4,12 +4,6 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//StringBuilder s= new StringBuilder();
-//s.append("we");
-//s.insert(0, "hello");
-//System.out.println(s.toString());
-//Integer a=Integer.valueOf(s);
-//System.out.println(a);
 		String str="sdjkasnsq";
 		String s2=str.replace("s","");
 		System.out.println(s2);
